c6ab36b11acb1bdd645f0d9f39697268d02ead80
Zach Horton <wz.horton@gmail.com>
2022-09-13 18:01:12 -0700

    add interface functions

---
+++ R/FME.R
@@ -0,0 +1,162 @@
+#### Functions interfacing to the FunctionalMixedEffect.jl moduls ####
+
+#' Set the \code{FME} config variable in the Julia environment.
+#'
+#' The argument names and values are saved to the \code{julia_obj}
+#' environment, then the config object is built.
+#'
+#' @param julia_obj Julia environment object from \code{julia_setup}.
+#' @param save_random_effects Logical indicating whether the random effects
+#' chain should be saved. Strict logical is required.
+#' @param niter,nburn,nthin MCMC iteration parameters. Strict integers are required.
+#'
+#' @return Invisibly returns list of config variables. The config object \code{cfg}
+#' is saved in the \code{julia_obj} environment.
+#' @export
+
+set_config_fme <- function(julia_obj, niter = 5000L, nburn = 1000L, nthin = 1L,
+                           save_random_effects = FALSE){
+  if(!all(is.integer(c(niter, nburn, nthin)))){
+    stop("niter, nburn, nthin must all be strict integers
+         (i.e. niter=5000L instead of =5000")
+  }
+  if(!is.logical(save_random_effects)){
+    stop("save_random_effects must be strict logical (i.e. TRUE/FALSE)")
+  }
+  arg_list <- as.list(environment())[-1]
+  for(i in seq_along(arg_list)){
+    julia_obj$assign(names(arg_list)[i], arg_list[[i]])
+  }
+  julia_obj$command("cfg = OutputConfigFME(niter, nburn, nthin, save_random_effects)")
+  invisible(arg_list)
+}
+
+
+#' Set the \code{FME} hyperparameters in the Julia environment.
+#'
+#' The argument names and values are save to the \code{julia_obj}
+#' environment, then the hyperparameter object is built. THe values must
+#' all return \code{TRUE} to \code{is.double}.
+#'
+#' @param julia_obj Julia environment object from \code{julia_setup}.
+#' @param a_sig,b_sig sigma priors. Must have \code{a > 2}
+#' @param a_tau,b_tau tau priors. Must have \code{a > 2}
+#' @param a_lam,b_lam lambda priors. Must have \code{a > 2}
+#' @param v_fix fixed effect prior variance.
+#'
+#' @return Invisibly returns list of hyperparameter variables. The hyperparameter
+#' object \code{hyps} is saved in the \code{julia_obj} environment.
+#' @export
+
+set_hyperparm_fme <- function(julia_obj, a_sig=3, b_sig=1, a_tau=3, b_tau=1,
+                              a_lam=3, b_lam=1, v_fix=1000){
+  if(!all(is.double(c(a_sig, b_sig, a_tau, b_tau,a_lam, b_lam, v_fix)))){
+    stop("hyperparameters must all be strict doubles")
+  }
+  arg_list <- as.list(environment())[-1]
+  for(i in seq_along(arg_list)){
+    julia_obj$assign(names(arg_list)[i], arg_list[[i]])
+  }
+  julia_obj$command("hyps = HyperParametersFME(a_sig, b_sig, a_tau, b_tau,
+                    a_lam, b_lam, v_fix)")
+  invisible(arg_list)
+}
+
+
+#' Functional mixed effects MCMC
+#'
+#' Pass the final model parameters and fit the model to data. \code{HyperParameter}
+#' and \code{OutputConfig} objects must be defined.
+#'
+#' @param julia_obj Julia environment from \code{julia_setup}.
+#' @param Yobs matrix containing curves/functions as columns.
+#' @param Xfix matrix containing fixed effect covariates, with individuals contained
+#' as columns and variables as rows (reverse of usual design matrices).
+#' @param Xrand matrix containing fixed effect covariates, with individuals contained
+#' as columns and variables as rows (reverse of usual design matrices). Can be
+#' set to \code{NULL} to perform functional regression without random effects.
+#' @param p number of spline coefficients. Must be strict integer (5L instead of 5).
+#'
+#' @return Invisibly returns list of argument values. The resulting object \code{chains}
+#' is saved in the \code{julia_obj} environment.
+#' @export
+
+mcmc_fme <- function(julia_obj, Yobs, Xfix, Xrand = NULL, p = 20){
+  if(!all(is.double(Yobs))){
+    stop("Yobs is not a matrix of doubles")
+  }
+  if(!all(is.double(Xfix))){
+    stop("Xfix is not a matrix of doubles")
+  }
+  if(!all(is.double(Xrand)) && !is.null(Xrand)){
+    stop("Xrand is neither NULL nor a matrix of doubles")
+  }
+  if(!is.integer(p)){
+    stop("p is not a strict logical")
+  }
+  if(!julia_obj$exists("cfg")){
+    stop("Config object not found")
+  }
+  if(!julia_obj$exists("hyps")){
+    stop("Hyperparameter object not found")
+  }
+  arg_list <- as.list(environment())[-1]
+  for(i in seq_along(arg_list)){
+    julia_obj$assign(names(arg_list)[i], arg_list[[i]])
+  }
+
+  julia_obj$command("chains = mcmc_fme(Yobs, Xfix, Xrand, p, hyps, cfg)")
+  invisible(arg_list)
+}
+
+
+#' Extract \code{FME} model chain objects into an R list
+#'
+#' Custom structs like the returned chains object don't return properly in R.
+#' This function extracts the elements into a list.
+#'
+#' @param julia_obj Julia environment object from \code{julia_setup} which has
+#' also run the \code{FME} MCMC code.
+#'
+#' @return List of chain objects. Note the special unicode characters in the
+#' Julia object are renamed using standard characters.
+#' @export
+
+extract_chains_fme <- function(julia_obj){
+  if(~julia$exists("chains")){
+    stop("No chains object found")
+  }
+  chains <- list()
+  chains$sigma <- julia_obj$eval("chains.σ")
+  chains$tau <- julia_obj$eval("chains.τ")
+  chains$lambda <- julia_obj$eval("chains.λ")
+  chains$Bfix <- julia_objs$eval("chains.Bfix")
+  chains$Brand <- julia_obj$eval("chains.Brand")
+  return(chains)
+}
+
+
+#' Functional mixed effects model wrapper function
+#'
+#' Fits the functional mixed effects model. This allows the user to provide all
+#' parameters at once rather than sequentially calling the interface functions.
+#'
+#' @param julia_obj A Julia environment from \code{julia_setup}.
+#' @param Yobs,Xfix,Xrand,p see documentation for \code{mcmc_fme}
+#' @param niter,nburn,nthin,save_random_effects see documentation for \code{set_config_fme}
+#' @param a_sig,b_sig,a_tau,b_tau,a_lam,b_lam,v_fix see documentation for \code{set_hyperparm_fme}
+#'
+#' @return MCMC chains object, see documentation for \code{mcmc_fme}.
+#' @export
+
+fit_model_fme <- function(julia_obj, Yobs, Xfix, Xrand = NULL, p = 20L,
+                          niter = 15000L, nburn = 5000L, nthin = 1L,
+                          save_random_effects = FALSE, a_sig = 3,
+                          b_sig = 1, a_tau = 3, b_tau = 1, a_lam = 3,
+                          b_lam = 1, v_fix = 1000){
+  set_config_fme(julia_obj, niter, nburn, nthin, save_random_effects)
+  set_hyperparm_fme(julia_obj, a_sig, b_sig, a_tau, b_tau, a_lam, b_lam, v_fix)
+  mcmc_fme(julia_obj, Yobs, Xfix, Xrand, p)
+  extract_chains_fme(julia_obj)
+}
+
